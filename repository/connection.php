8367189d45f365fb9a705a744f93950fef1ad3b0
Wizzi.GameDev <105589023+WizziGameDev@users.noreply.github.com>
2024-11-05 08:07:07 +0700

    Update connection.php

--- repository/connection.php
+++ repository/connection.php
@@ -1,11 +1,11 @@
 <?php
 function getConnection(): PDO
 {
-    $host = "localhost";
+    $host = "db";
     $port = 3306;
     $database = "fp_web";
     $username = "root";
-    $password = "";
+    $password = "12345";
 
     return new PDO("mysql:host=$host;port=$port;dbname=$database", $username, $password);
 }
